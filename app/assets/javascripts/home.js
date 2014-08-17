8b6fc21c4ff5fbee2c4734ac0a22163d5d49ddaf
Lucas Churchill <lucas@agtlucas.com>
2014-08-17 18:57:12 -0300

    Updates...

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,1 +1,31 @@
-window.viewportUnitsBuggyfill.init();
+// /**
+//  * Home Scripts
+//  * @author Lucas <lucas@lucas.ninja>
+//  */
+
+// var app = (function (window, document, undefined) {
+
+//   'use strict';
+
+//   /*
+//    * App object
+//    */
+//   var app = {};
+
+//   /*
+//    * Init method
+//    * Initialize other methods
+//    */
+//   app.init = function () {
+//     this.iosViewportBug();
+//   };
+
+//   /*
+//    * Fix iOS vh unit bug
+//    * Using viewportUnitsBuggyfill plugin https://github.com/rodneyrehm/viewport-units-buggyfill
+//    */
+//   app.iosViewportBug = function () {
+//     window.viewportUnitsBuggyfill.init();
+//   };
+
+// })(window, document);
