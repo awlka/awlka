8f08fc1613e54a5759b6a25256ae3d24b5bdf50e
Lucas Churchill <lucas@agtlucas.com>
2014-08-26 08:46:19 -0300

    Adding contact model

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < MailForm::Base
+  attribute :name,    :validate => true
+  attribute :email,   :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :telephone
+  attribute :message
+
+  def headers
+    {
+      :subject => "Contato no site AWLKA",
+      :to => "contato@awlka.com",
+      :from => %("#{name}" <#{email}>),
+      :'reply-to' => "#{email}"
+    }
+  end
+
+end
