94addc709557ed5d52ddd0ea6bbce6fff7f3e89f
Lucas Churchill <lucas@agtlucas.com>
2014-08-26 08:46:38 -0300

    Update Contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,17 @@
 class ContactsController < ApplicationController
+
+  def index
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(:params[contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = "Ok!"
+    else
+      flash.now[:notice] = "Not ok at all!"
+    end
+  end
+
 end
