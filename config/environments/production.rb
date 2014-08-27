98865acfa6a3c270a1aa3c141543b7ab686bf052
Lucas Churchill <lucas@agtlucas.com>
2014-08-27 10:27:49 -0300

    Update production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,23 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.action_mailer.default_url_options = { :host => 'awlka-delta.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'awlka-delta.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
