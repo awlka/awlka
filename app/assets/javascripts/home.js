73c2afe3584f663bdaa2951dd871bb3012df51ad
Lucas Churchill <lucas@agtlucas.com>
2014-09-09 09:25:45 -0300

    preload none

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -35,9 +35,9 @@ var app = (function (window, document, undefined) {
    * Append video on home if isn't Mobile
    */
   app.appendVideo = function () {
-    var theVideo = '<video class="the-video" preload="auto" autoplay loop poster="black-ink.jpg" width="100%" height="100%"><source src="black-ink.webm"></source><source src="black-ink-min.mp4"></source></video>';
-    window.onload = function () {
-      if (!app.isMobile()) {
+    var theVideo = '<video class="the-video" preload="none" autoplay loop poster="black-ink.jpg" width="100%" height="100%"><source src="black-ink.webm"></source><source src="black-ink-min.mp4"></source></video>';
+    if (!this.isMobile()) {
+      window.onload = function () {
         $('.presentation-container__video').append(theVideo);
       }
     };
